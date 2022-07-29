d288be1ff131e0ba96bb14f8eb7ef35536761963
Zachary Vorhies <zachvorhies@protonmail.com>
2022-07-28 21:42:24 -0700

    Update install_ubuntu22.sh

--- install_ubuntu22.sh
+++ install_ubuntu22.sh
@@ -1,3 +1,6 @@
+# Install this with:
+# curl -sL https://raw.githubusercontent.com/zackees/webtorrent-how-to-seed-server/main/install_ubuntu22.sh | sudo -E bash -
+
 apt update
 apt install python3 python3-pip npm nodejs -y
 pip install magic-wormhole
