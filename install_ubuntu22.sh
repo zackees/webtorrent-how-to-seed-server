1fc6a31613f039766e17a13e74bb16903952fe81
Zachary Vorhies <zachvorhies@protonmail.com>
2022-07-28 21:35:59 -0700

    Create install_ubuntu22.sh

---
+++ install_ubuntu22.sh
@@ -0,0 +1,7 @@
+apt update
+apt install python3 python3-pip npm nodejs -y
+pip install magic-wormhole
+
+# Installs webtorrent components
+npm install -g node-pre-gyp webtorrent-cli webtorrent-hybrid
+
